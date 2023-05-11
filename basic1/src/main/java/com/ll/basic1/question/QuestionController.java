f9d79091e8d08fb9704256d326cb5402fee8574b
SeungYeol91 <irenesa1126@gmail.com>
2023-05-11 14:28:36 +0900

    0511

--- basic1/src/main/java/com/ll/basic1/question/QuestionController.java
+++ basic1/src/main/java/com/ll/basic1/question/QuestionController.java
@@ -2,13 +2,15 @@ package com.ll.basic1.question;
 
 import java.util.List;
 
+import com.ll.basic1.answer.AnswerForm;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -18,15 +20,27 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
     }
 }
--- basic1/src/test/java/com/ll/basic1/Basic1ApplicationTests.java
+++ basic1/src/test/java/com/ll/basic1/Basic1ApplicationTests.java
@@ -1,36 +1,23 @@
 package com.ll.basic1;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
-import com.ll.basic1.answer.Answer;
-import com.ll.basic1.question.Question;
-import com.ll.basic1.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.ll.basic1.question.QuestionService;
 
 @SpringBootTest
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
-    @Transactional // 테스트 때만 발생. 재기동
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(2); // 질문 아이디가 2값인거 oq에 넣고
-        assertTrue(oq.isPresent()); // 존재한다면
-        Question q = oq.get(); // q에 oq 불러오고
-
-        List<Answer> answerList = q.getAnswerList(); // q에 답변리스트 불러오고
-
-        assertEquals(1, answerList.size()); // 답변리스트 크기가 1이냐
-        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent()); // 답변이 ""냐
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
     }
 }
